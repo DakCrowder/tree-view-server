9e1ed08cf0d3de6a4be6306ffd0dbe0c11684228
Dakota Crowder <>
2018-11-08 10:14:49 -0500

    Configure sequelize for heroku connection

--- sequelize.js
+++ sequelize.js
@@ -4,16 +4,29 @@ const FactoryModel = require('./models/factory')
 const NodeModel = require('./models/node')
 const RootModel = require('./models/root')
 
-const sequelize = new Sequelize('tree_view', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-})
+let sequelize = null
+
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect:  'mysql',
+    protocol: 'mysql',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  })
+} else {
+  sequelize = new Sequelize('tree_view', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  })
+}
 
 sequelize
   .authenticate()
